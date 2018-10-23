4ffc2591f6a7e3c72a8bccde04c7ac981d6e95fc
samfar5161 <39602974+samfar5161@users.noreply.github.com>
2018-10-23 14:19:20 -0400

    Initial react_samash

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './pagedraw/header';
+import GuitarLink from './pagedraw/maincontent_deptcat_guitar';
+import BassLink from './pagedraw/maincontent_deptcat_bass';
+import DrumLink from './pagedraw/maincontent_deptcat_drums';
+import {Route, NavLink, HashRouter} from 'react-router-dom';
+import AmpLink from './pagedraw/maincontent_deptcat_amp';
+import LiveLink from './pagedraw/maincontent_deptcat_livesound';
+import RecordLink from './pagedraw/maincontent_deptcat_recording';
+import KeyLink from './pagedraw/maincontent_deptcat_keys';
+import AccessoryLink from './pagedraw/maincontent_deptcat_accessories';
+import MicLink from './pagedraw/maincontent_deptcat_mic';
+import Guitarcontent from './pagedraw/guitarcontent_maincontainer';
+import Basscontent from './pagedraw/basscontent_maincontainer';
 
 class App extends Component {
+  constructor(props){
+    super()
+
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <HashRouter>
+          <div>
+            <div className="Header">
+
+            </div>
+            <div className="Navigation">
+              <ul className="navList1">
+                <li><NavLink to="/pagedraw/guitarcontent_maincontainer"><GuitarLink/></NavLink></li>
+                <li><NavLink to="/pagedraw/basscontent_maincontainer"><BassLink/></NavLink></li>
+                <li><DrumLink/></li>
+              </ul>
+              </div>
+              <div className="content1">
+                <Route path="/pagedraw/guitarcontent_maincontainer" component={Guitarcontent}/>
+                <Route path="/pagedraw/basscontent_maincontainer" component={Basscontent}/>
+              </div>
+            </div>
+          </HashRouter>
+        </div>
     );
   }
 }
